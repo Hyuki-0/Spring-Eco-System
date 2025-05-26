18afff95ebeac587f000728300cfe7b89f216a75
Hyuki-0 (Hyuki) <soohyuk96@daum.net>
2025-05-26 23:06:49 +0900

    [feat] : Clock을 활용한 고정/특정 시간대 대역 테스트

---
+++ src/test/java/com/hyuki/tobi_spring/learningtest/ClockTest.java
@@ -0,0 +1,42 @@
+package com.hyuki.tobi_spring.learningtest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClockTest {
+
+  // Clock 이용해서 LocalDateTime.now
+  @Test
+  void clock() {
+    Clock clock = Clock.system(ZoneId.of("UTC")); // 서버 시간대
+
+    LocalDateTime now = LocalDateTime.now(clock);
+    LocalDateTime now2 = LocalDateTime.now(clock);
+
+    assertThat(now).isBeforeOrEqualTo(now2);
+  }
+
+  // Clock Test에서 사용할 때 내가 원하는 시간을 지정해서 현재 시간을 가져오게 할 수 있는가?
+  @Test
+  @DisplayName("fixedClock")
+  void fixedClock(){
+    // given
+    // fixed 사용 시, 시간을 고정해둘 수 있다.
+    Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+    // when
+    LocalDateTime now = LocalDateTime.now(clock);
+    LocalDateTime now2 = LocalDateTime.now(clock);
+
+    // then
+    assertThat(now2).isEqualTo(now);
+  }
+}
+
--- src/test/java/com/hyuki/tobi_spring/payment/PaymentServiceSpringTest.java
+++ src/test/java/com/hyuki/tobi_spring/payment/PaymentServiceSpringTest.java
@@ -6,6 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.hyuki.tobi_spring.TestObjectFactory;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +22,21 @@ public class PaymentServiceSpringTest {
   @Autowired
   PaymentService paymentService;
 
+  @Autowired
+  ExRateProviderStub exRateProviderStub;
+
+
   @Test
   void convertedAmount() throws IOException, InterruptedException {
+    // exRate: 1000
     Payment pa = paymentService.prepare(1L, "USD", BigDecimal.TEN);
     assertThat(pa.getExRate()).isEqualByComparingTo(valueOf(1_000));
+
+    //exRate: 500
+    exRateProviderStub.setExRate(valueOf(500));
+    Payment pa2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+    assertThat(pa2.getExRate()).isEqualByComparingTo(valueOf(500));
+    assertThat(pa2.getConvertedAmount()).isEqualByComparingTo(pa2.getExRate().multiply(BigDecimal.TEN));
   }
 }
